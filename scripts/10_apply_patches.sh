7347b84d91450c625e71a881b95e226741dad676
Tomy <tomywang999@gmail.com>
2024-06-27 18:14:56 +0800

    chore: Remove unused scripts and patches

--- scripts/10_apply_patches.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "[*] Applying patches"
-for PATCHFILE in patches/*.patch patches/${MODEL}/*.patch; do
-  echo ">> ${PATCHFILE}"
-  patch -p1 --no-backup-if-mismatch -r /dev/null -d ${ROOTFS} < ${PATCHFILE}
-done
--- scripts/11_ssh_keys.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "[*] Adding symlink to ssh-keys in data"
-ln -vsf /data/ssh $ROOTFS/root/.ssh
-#ln -vsf /data/ssh $ROOTFS/etc/dropbear
